f6e98017d4b1f25607b050f43a2a3e46e85675a7
liame1 <liamesler47@gmail.com>
2025-03-03 23:32:38 -0800

    got modal button to work net art project1

--- popup-test/js/main.js
+++ popup-test/js/main.js
@@ -1,13 +1,13 @@
 // Get the modal
-var modal = document.getElementById("my-modal");
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("my-btn");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
@@ -22,5 +22,4 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
-
+} 
