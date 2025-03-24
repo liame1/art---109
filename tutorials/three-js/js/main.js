f4fe247329787b1210ede142f850f47942285122
liame1 <liamesler47@gmail.com>
2025-03-24 12:45:48 -0700

    added files from three.js tutorial
    need to follow tutorial, a little confusing so far, I think I will just follow the video first as I believe it will help me understand.

---
+++ tutorials/three-js/js/main.js
@@ -0,0 +1,47 @@
+// Basic Three.JS scene from documentation, importing Three.JS through a CDN 
+// https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
+
+
+//~~~~~~~Import Three.js (also linked to as import map in HTML)~~~~~~
+import * as THREE from 'three';
+
+// Import add-ons
+import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
+// import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js'; // to load 3d models
+
+
+
+// ~~~~~~~~~~~~~~~~Create scene here~~~~~~~~~~~~~~~~
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop( animate );
+document.body.appendChild(renderer.domElement);
+
+
+
+// ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
+const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new GLTFLoader(); // to load 3d models
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+
+}
+
+
+// →→→→→→ Follow next steps in tutorial: // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
+
+
