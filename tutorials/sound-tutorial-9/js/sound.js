1ff75184913d185b5e6ee1237185f233e2c89db1
liame1 <liamesler47@gmail.com>
2025-04-15 01:09:54 -0700

    sound tutorial 9!

---
+++ tutorials/sound-tutorial-9/js/sound.js
@@ -0,0 +1,31 @@
+let song = document.querySelector("#pluggnb");
+
+let playBtn = document.querySelector("#play-btn");
+let pauseBtn = document.querySelector("#pause-btn");
+
+let volumeUp = document.querySelector("#volume-up");
+let volumeDown = document.querySelector("#volume-down");
+
+// ----- BUTTONS :
+playBtn.addEventListener('click', function() {
+    song.play();
+    console.log("song played");
+});
+pauseBtn.addEventListener('click', function() {
+    song.pause();
+    console.log("song paused");
+});
+volumeUp.addEventListener('click', function() {
+    song.volume += 0.1;
+    console.log("volume raised");
+});
+volumeDown.addEventListener('click', function() {
+    song.volume -= 0.1;
+    console.log("volume lowered");
+});
+
+// ------- DISPLAY AFTER SONG LOADED :
+song.onloadeddata = function() {
+    playBtn.style.visibility = "visible";
+    console.log("sound loaded");
+}
